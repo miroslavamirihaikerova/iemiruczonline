7d6d494f592c26a84e8bdcfb47697545ac035ab1
Miroslava Miri Haikerová <210729774+miroslavamirihaikerova@users.noreply.github.com>
2025-10-02 13:27:52 +0200

    Update page.tsx
    Doplnění sociálních sítí.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white p-4">
-      <img
-        src="/obrazek.jpg"
-        alt="Hero image"
-        className="max-w-xl rounded-xl shadow-lg mb-8"
+    
       />
       <h1 className="text-3xl font-bold mt-2">Total game over. Datový vortex zpracován na 111%.</h1>
       <p className="text-center max-w-md mb-6">
@@ -14,7 +10,7 @@ export default function HomePage() {
       <div className="flex gap-4">
         <a
           href="https://facebook.com/miroslavamirihaikerovaeu"
-          className="text-blue-400 hover:underline"
+          className="text-lila-400 hover:underline"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -22,7 +18,7 @@ export default function HomePage() {
         </a>
         <a
           href="https://instagram.com/miroslavamirihaikerova"
-          className="text-pink-400 hover:underline"
+          className="text-orange-400 hover:underline"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -37,11 +33,12 @@ export default function HomePage() {
           LinkedIn
         </a>
         <a
-          href="https://pinterest.com/in/miroslavamirihaikerovaeu"
+          href="https://x.com/linka.digital"
           className="text-red-400 hover:underline"
           target="_blank"
           rel="noopener noreferrer"
-      
+      >
+          X
         </a>
        
       </div>
